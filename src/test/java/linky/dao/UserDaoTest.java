0b14eafdcb6398d1e62408c555e66179ee230535
mihails.krasikovs <mihails.krasikovs@ideaportriga.lv>
2017-07-01 17:01:12 +0300

    adding UserDaoTest as an example of integration tests

---
+++ src/test/java/linky/dao/UserDaoTest.java
@@ -0,0 +1,33 @@
+package linky.dao;
+
+import linky.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserDaoTest {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Test
+	public void create() {
+		User user = new User("batman@batman.com", "secret", "Bruce Wayne");
+		userDao.save(user);
+
+		Iterable<User> users = userDao.findAll();
+		assertThat(users).hasSize(1);
+		User savedUser = users.iterator().next();
+		assertThat(savedUser.id()).isNotNull();
+		assertThat(savedUser.createdAt()).isNotNull();
+		assertThat(savedUser.email()).isEqualTo("batman@batman.com");
+		assertThat(savedUser.password()).isEqualTo("secret");
+		assertThat(savedUser.name()).isEqualTo("Bruce Wayne");
+	}
+}
