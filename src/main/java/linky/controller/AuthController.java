e99df8c7bb94ff96c65ef37220d7e499ae65db88
mihails.krasikovs <mihails.krasikovs@ideaportriga.lv>
2017-07-19 12:22:42 +0300

    rename to AuthController

--- src/main/java/linky/controller/AuthController.java
+++ src/main/java/linky/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MainController {
+public class AuthController {
 
 	@Autowired
 	private PipedNow pipedNow;
